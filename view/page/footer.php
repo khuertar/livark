5698e919eb95026ccc10c85575a0aabe2fc2e369
Manuel <m.ramirez@ignilytic.com>
2021-08-23 15:03:06 -0500

    Se creo el footer

---
+++ view/page/footer.php
@@ -0,0 +1,44 @@
+<!--===============================================
+Footer
+=================================================-->
+<footer>
+  <div class="in-footer">
+    <div class="footer-top">
+      <div class="logo">
+        <a href="inicio"><img src="view/img/logoBlancoRojo.svg" alt="logo"></a>
+      </div>
+      <div class="footer-redes">  
+        <a href="https://youtube.com/channel/UCQNrNWRm-vAMiFIxZFioPag"><img src="view/img/youtube.svg" alt="youtube"></a>
+        <a href="https://instagram.com/grupo_livark?utm_medium=copy_link"><img src="view/img/instagram.svg" alt="instagram"></a>
+        <a href="https://www.facebook.com/Livark/"><img src="view/img/face.svg" alt="facebook"></a>
+      </div>
+    </div>
+    <div class="footer-sep"></div>
+    <div class="footer-boot">
+      <div class="footer-boot-item">
+        <span>Links Extras</span>
+          <div class="contenido">
+            <a href="#">Politica de privacidad</a>
+        </div>
+      </div>
+      <div class="footer-boot-item">
+        <span>Acceso Rapido</span>
+          <div class="contenido">
+            <a href="inicio">Inicio</a>
+            <a href="empresa">Empresa</a>
+            <a href="portafoli">Portafolio</a>
+            <a href="contacto">Contacto</a>
+          </div>
+      </div>
+      <div class="footer-boot-item">
+        <span>Nuestros Horarios</span>
+        <div class="contenido">
+          <p>Lunes - Viernes: 9AM - 6PM</p>
+          <p>Sábado - Domingo: Cerrado</p>
+          <p>Teléfono: 00-000-000</p>
+          <p>Correo: contacto@livark.com.mx</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
