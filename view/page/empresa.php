750f24fe85a4aadb2c877b69bcf02e5f94a40995
Manuel <m.ramirez@ignilytic.com>
2021-08-23 22:31:27 -0500

    Se agrego la parte de empresa

---
+++ view/page/empresa.php
@@ -0,0 +1,163 @@
+<section class="empresa">
+<!--===============================================
+Empresa
+=================================================-->
+  <div class="txt-empresa">
+    <h2>Empresa</h2>
+    <div class="txt">
+      <p>
+        <b>Grupo Livark</b> se fundó en el año 2014. Somos una empresa comprometida en resolver temas integrales, optimizamos en un corto plazo las necesidades
+        que nuestros clientes nos presentan, permitiendo la reducción de costos, una mejora tecnológica y visual.
+        <br><br>
+        Con esto damos un soporte de confianza y comunicación para la correcta toma de decisiones dentro del proyecto.
+        <br><br>
+        Damos a nuestros proyectos la transparencia, el compromiso y esfuerzo necesarios con base en los principios fundamentales de donde parte nuestra      empresa, haciendo de las necesidades de los clientes nuestras necesidades.
+        <br><br>
+        La satisfacción del cliente es primordial para nuestro equipo de trabajo, mantenemos una armonía dentro de la organización que beneficie a cada      proyecto obteniendo resultados de excelencia.
+      </p>
+      <img class="img-fluid" src="view/img/edificios_emp.jpg" alt="edificio">
+    </div>
+  </div>
+
+  <div class="separacion-empresa"></div>
+
+<!--===============================================
+Presentacion
+=================================================-->
+  <div class="presentacion">
+    <h2>Presentación</h2>
+    <div class="txt-pre">
+      <p>
+      Desde principios del 2014 en Grupo Livark estamos comprometidos con el ramo de la construcción, creamos espacios retomando los mejores elementos del diseño e imagen,llevamos las ideas del cliente a su realización mediante una libre arquitectura.
+      <br><br>
+      Somos una empresa joven y mantenemos intacta la emoción del primer proyecto,
+      generando ideas frescas e innovadoras que optimicen los recursos, creamos propuestas de valor para los planes de renovación, adaptación, uso de arquitectura sustentable y adaptación tecnológica.
+      <br><br>
+      En está presentación encontrará una descripción general de la empresa, los
+      servicios que ofrecemos, algunos de nuestros proyectos realizados e incluso soluciones a sus necesidades.
+      </p>
+      <img src="view/img/edi-nubes.jpg" alt="edificio">
+    </div>
+  </div>
+
+<!--===============================================
+Livark
+=================================================-->
+  <div class="livark">
+    <div class="livark-izq">
+        <p>¿POR QUÉ LIVARK?</p>
+        <img src="view/img/logo_blanco.svg" alt="edificio">
+    </div>
+    <div class="livark-der">
+      En México lo primero que pensamos que al contratar a un arquitecto “es muy caro”,
+      sin embargo puede estar más alejado de la realidad, contratarlo no necesariamente
+      implica que una obra vaya a ser cara,muchas veces lo barato sale caro y los
+      proyectos mal llevados pueden traer mayores inconvenientes.
+      <br><br>
+      Nuestra empresa ofrece servicios de calidad, optimizando costos y tiempos en
+      los procesos constructivos y de diseño.
+      <br><br>
+      Tenemos la experiencia que nuestros clientes necesitan para lograr sus objetivos, sin necesidad de interferir con sus procesos productivos durante las a decuaciones en la empresa mientras prestamos nuestros servicios ofreciendo calidad y la mejor  solución a las necesidades que usted requiere.
+      <br><br>
+      Utilizamos el Project Management como la base de nuestro trabajo brindamos soluciones propias para los requerimientos del proyecto que nos permitan generar una relación a largo plazo con su empresa.
+    </div>
+    <img class="circulo" src="view/img/edi-porque.jpg" alt="edificio">
+  </div>
+
+  <div class="separacion-empresa"></div>
+
+<!--===============================================
+Valores
+=================================================-->
+  <div class="valores">
+    <div class="valores-izq"></div>
+    <div class="valores-der">
+      <p>
+        <span>¿QUÍENES SOMOS?</span>         
+        Somos una empresa comprometida en resolver los temas inmobiliarios del cliente con una plataforma de servicios que cubren las problemáticas más importantes que enfrentan los profecionales y las empresas.
+        
+        <span>NUESTRA MISIÓN</span>         
+        
+        Crear la pronta resolución para las necesidades arquitectónicas y de
+        construcción brindando la mejor calidad, ética, respuesta, cooperación profecional e inivación del mercado cubriendo las necesidades del cliente en el menor plazo.
+        
+        <span>NUESTRA VISIÓN</span> 
+        
+        Consolidarnos como la empresa líder en servicios de construcción y diseño a través de la confianza, innovación y adaptación para lograr
+        la mayor satisfación de nuestros clientes.
+        
+        Nuestro personal se encuentra capacitado con adecuada formación académica y exelente experiencia en los diferentes campos de la construccíon y comercialización, está consta de “arquitectos, diseñadores de interiores,
+        gráficos”, especialistas en cada diciplina de ingenerías y diseño con el fin de ofrecer el mejor servicio en el mercado.
+      </p>
+    </div>
+  </div>
+
+  <div class="separacion-empresa"></div>
+
+<!--===============================================
+Magnamet
+=================================================-->
+<div class="magnament">
+  <h2>MAGNAMENT</h2>
+  <div class="in-magnament">
+    <div class="in-magnament-txt">
+      <p>Disciplina la cual abarca:</p>
+      <ul>
+        <li>Planeación</li>
+        <li>Organización</li>
+        <li>Integración</li>
+        <li>Dirección</li>
+        <li>Control</li>
+      </ul>
+    </div>
+    <div class="in-magnament-img"></div>
+  </div>
+  <div class="magnament-txt">
+    <p>
+    Con la finalidad de alcanzar los objetivos propuestos para lograr el éxito en cada proyecto en tiempo, forma, calidad y apegados al presupuesto.
+    <br><br>
+    Ya que con esto tenemos un alcance controlado, una comunicación constante y efectiva, un plan de proyecto el cual logra objetivos, delega responsabilidades y mide el progreso. De está forma se distribuyen todos los recursos disponibles, para lograr el objetivo en común con las metodologías técnicas, procesos, estructura interna y herramientas, entre otros.
+    </p>
+  </div>
+</div>
+
+<!--===============================================
+Testimoniales
+=================================================-->
+<div class="testimoniales">
+  <h2>TESTIMONIALES</h2>
+  <div class="owl-carousel owl-theme carrusel">
+    <div class="item">
+      <div class="testimonial--item-cara"></div>
+      <span>Juan Perez <br> Ceo - La Poblana</span>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates officiis tenetur delectus? Provident hic asperiores ab, cupiditate in nemo neque necessitatibus reiciendis, repudiandae possimus, quae error minima sit maxime assumenda.
+      </p>
+    </div>
+    <div class="item">
+      <div class="testimonial--item-cara"></div>
+      <span>Juan Perez <br> Ceo - La Poblana</span>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates officiis tenetur delectus? Provident hic asperiores ab, cupiditate in nemo neque necessitatibus reiciendis, repudiandae possimus, quae error minima sit maxime assumenda.
+      </p>
+    </div>
+    <div class="item">
+      <div class="testimonial--item-cara"></div>
+      <span>Juan Perez <br> Ceo - La Poblana</span>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates officiis tenetur delectus? Provident hic asperiores ab, cupiditate in nemo neque necessitatibus reiciendis, repudiandae possimus, quae error minima sit maxime assumenda.
+      </p>
+    </div>
+    <div class="item">
+      <div class="testimonial--item-cara"></div>
+      <span>Juan Perez <br> Ceo - La Poblana</span>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates officiis tenetur delectus? Provident hic asperiores ab, cupiditate in nemo neque necessitatibus reiciendis, repudiandae possimus, quae error minima sit maxime assumenda.
+      </p>
+    </div>
+      
+  </div>
+</div>
+
+
+</section>
