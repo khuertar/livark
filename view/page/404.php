d790fc917b52b96ca1c9b776559dfda084c09267
Manuel <m.ramirez@ignilytic.com>
2021-08-23 22:40:08 -0500

    Se creo pagina de error404

---
+++ view/page/404.php
@@ -0,0 +1,8 @@
+<!--===============================================
+Error 404
+=================================================-->
+<section class="error">
+  <h1>404 Página web no encontrada</h1>
+  <h3>Por favor verifique su link</h3>
+  <div class="btn-error"><a href="inicio">Link oficial</a></div>
+</section>
